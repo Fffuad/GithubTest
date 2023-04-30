2fc9a26cd26a01cb9d7071440ec492f5c18b2127
MSI <fuadbayramov.fb@gmail.com>
2023-04-30 10:46:35 -0800

    6ci

--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,6 @@
-package PACKAGE_NAME;public class Person {
+public class Person {
+    public static void main(String[] args) {
+        System.out.println("ghgchg");
+        System.out.println("fibddsbvibv");
+    }
 }
